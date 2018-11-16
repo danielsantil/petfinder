d0cf95dec766e5a7c29910fd666fe363f9ebf29f
Daniel Santil <daniel.shcf@gmail.com>
2018-11-15 21:11:34 -0400

    added method to check internet connection

--- app/src/main/java/com/androidadvanced/petfinder/activities/BaseActivity.java
+++ app/src/main/java/com/androidadvanced/petfinder/activities/BaseActivity.java
@@ -1,5 +1,8 @@
 package com.androidadvanced.petfinder.activities;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -11,4 +14,13 @@ public abstract class BaseActivity extends AppCompatActivity {
     }
 
     protected abstract String getActivityName();
+
+    protected boolean isConnectedToInternet() {
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (cm != null) {
+            networkInfo = cm.getActiveNetworkInfo();
+        }
+        return networkInfo != null && networkInfo.isConnectedOrConnecting();
+    }
 }
