c5ca64afee1130b5b556c00f5ce957345dfd6090
Daniel Santil <daniel.shcf@gmail.com>
2018-11-15 21:14:37 -0400

    creating authentication profile model

---
+++ app/src/main/java/com/androidadvanced/petfinder/models/AuthProfile.java
@@ -0,0 +1,63 @@
+package com.androidadvanced.petfinder.models;
+
+import android.net.Uri;
+
+public class AuthProfile {
+    private String uid;
+    private String displayName;
+    private Uri photoUrl;
+    private long creationTime;
+    private String phoneNumber;
+    private String email;
+
+    public AuthProfile() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Uri getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(Uri photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public long getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(long creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
